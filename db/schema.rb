199beb0cef35a790d706e6b871b0f04e82a8f2ed
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-01-20 10:29:56 -0600

    added reference migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120162317) do
+ActiveRecord::Schema.define(version: 20160120162916) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 20160120162317) do
     t.integer  "sale"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
