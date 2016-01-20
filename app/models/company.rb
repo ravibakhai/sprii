e1b5fba68e11d616b9393c9f144300b7cc82ffd1
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-01-20 13:34:57 -0600

    associations for followers

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,9 @@
 class Company < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_followable
+  acts_as_follower
+  
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :posts 
+  acts_as_followable
+  acts_as_follower
+  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
