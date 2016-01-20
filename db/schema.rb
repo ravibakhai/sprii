e55ae441d07bf553e55750cde9e2265dbb1d630d
Ravi Bakhai <ravi.bakhai@gmail.com>
2016-01-20 10:33:13 -0600

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120162916) do
+ActiveRecord::Schema.define(version: 20160120163027) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(version: 20160120162916) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "company_id"
   end
 
+  add_index "posts", ["company_id"], name: "index_posts_on_company_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
